96412b9dfe47f85ac43e32003ed686ec2308f9bf
Ekaterina <reikon@list.ru>
2022-05-13 15:12:20 +0300

    file1

---
+++ MaxMin.java
@@ -0,0 +1,13 @@
+public class MaxMin {
+    public static void main(String[] args) {
+        System.out.println("Hello from Sales");
+        Byte bt1 = 127;
+        Short sh1 = 11;
+        Integer in1 = 12;
+        Long l1 = 13L;
+        Float fl1 = 14.5F;
+        Double db = 16.6;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+    }
+}
